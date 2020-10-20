9c3a29ab293373f26ba04bbf47245728c791a248
Justin Movitz <movitzj1@montclair.edu>
2020-10-20 18:21:32 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+def using_concat (array1, array2)
+  new_array = array1.concat(array2)
+  return new_array
+end
+  
+def using_insert(array, element)
+  new_array = array.insert(4, element)
+  return new_array
+end
